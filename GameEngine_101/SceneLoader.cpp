c00123c9542ea8fd06f6f47eb4c302d942c80d59
Jorge <jorge.amengol@gmail.com>
2017-11-30 18:47:51 -0400

    Remove some commented code

--- GameEngine_101/SceneLoader.cpp
+++ GameEngine_101/SceneLoader.cpp
@@ -46,19 +46,11 @@ void LoadModelsIntoScene(void)
     g_vecGameObjects.at(9)->position = glm::vec3(-40.0f, -6.2f, -264.0f);
     g_vecGameObjects.at(10)->position = glm::vec3(52.0f, -7.1f, -263.9f);
     g_vecGameObjects.at(11)->position = glm::vec3(-52.0f, -7.5f, -263.0f);
-    g_vecGameObjects.at(12)->position = glm::vec3(62.0f, -8.0f, -263.0f);
-    g_vecGameObjects.at(13)->position = glm::vec3(-62.1f, -8.3f, -263.0f);
-    g_vecGameObjects.at(14)->position = glm::vec3(70.0f, -9.0f, -261.0f);
-    g_vecGameObjects.at(15)->position = glm::vec3(-69.0f, -8.8f, -263.0f);
-    g_vecGameObjects.at(16)->position = glm::vec3(79.0f, -9.0f, -242.0f);
-    /*g_vecGameObjects.at(17)->position = glm::vec3(-80.0f, -9.0f, -242.1f);
-    g_vecGameObjects.at(18)->position = glm::vec3(85.0f, -9.7f, -232.0f);
-    g_vecGameObjects.at(19)->position = glm::vec3(-84.0f, -10.0f, -232.0f);
-    g_vecGameObjects.at(20)->position = glm::vec3(92.0f, -15.3f, -222.0f);
-    g_vecGameObjects.at(21)->position = glm::vec3(-93.0f, -15.0f, -221.5f);
-    g_vecGameObjects.at(22)->position = glm::vec3(100.0f, -15.5f, -210.2f);
-    g_vecGameObjects.at(23)->position = glm::vec3(-102.0f, -15.8f, -208.0f);
-    g_vecGameObjects.at(24)->position = glm::vec3(110.0f, -16.0f, -200.0f);*/
+    g_vecGameObjects.at(12)->position = glm::vec3(62.0f, -8.0f, -230.0f);
+    g_vecGameObjects.at(13)->position = glm::vec3(-62.1f, -8.3f, -222.0f);
+    g_vecGameObjects.at(14)->position = glm::vec3(70.0f, -9.0f, -220.0f);
+    g_vecGameObjects.at(15)->position = glm::vec3(-69.0f, -8.8f, -190.0f);
+    g_vecGameObjects.at(16)->position = glm::vec3(79.0f, -9.0f, -180.0f);
 
     // End of Vipers creation
     //=========================================================================
