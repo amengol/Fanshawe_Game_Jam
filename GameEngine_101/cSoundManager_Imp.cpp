9bd65ca3d4be911818a98db8ec1ad187e49b41e6
Jorge <jorge.amengol@gmail.com>
2017-12-05 14:01:22 -0400

    Add functionalities to better handle config file and auto create sound objects

--- GameEngine_101/cSoundManager_Imp.cpp
+++ GameEngine_101/cSoundManager_Imp.cpp
@@ -52,23 +52,12 @@ void cSoundManager_Imp::loadSoundObjectsIntoScene()
 
 void cSoundManager_Imp::initSoundScene()
 {
-
+    // Load sound configuration file
     if (!loadSoundParameters("../sounds/sound.conf"))
     {
         std::cout << "Something went wrong while loading the sound configuration!\n";
-    }
-
-    // Create the sounds and channels
-    const int NUM_OF_ELEMENTS = 2;
-
-    for (int i = 0; i < NUM_OF_ELEMENTS; i++)
-    {
-        FMOD::Sound* tempSd;
-        this->msounds.push_back(tempSd);
-        FMOD::Channel* tempCh;
-        this->mchannels.push_back(tempCh);
-        cSoudObject* tempSO;
-        this->soundObjects.push_back(tempSO);
+        system("pause");
+        exit(-1);
     }
 
     init_fmod();
@@ -107,46 +96,74 @@ void cSoundManager_Imp::initSoundScene()
     //mresult = dspflange->setBypass(true);
     //errorcheck(mresult);
 
+    // Sounds initializations
+    for (int i = 0; i < this->soundObjects.size(); i++)
+    {
+        FMOD::Sound* tempSd;
+        this->msounds.push_back(tempSd);
+        FMOD::Channel* tempCh;
+        this->mchannels.push_back(tempCh);
 
-    mresult = msystem->createSound(this->soundObjects[0]->getSource().c_str(), this->soundObjects[0]->getFMODType(), 0, &msounds[0]);
-    errorcheck(mresult);
-    mresult = msounds[0]->setMode(FMOD_LOOP_NORMAL);
-    errorcheck(mresult);
+        if (soundObjects.at(i)->getFMODType() == FMOD_3D)
+        {
+            //we need position and velocity vecors for 3d sounds
+            glm::vec3 soundPos = this->soundObjects[i]->getPosition();
+            FMOD_VECTOR sound_position;
+            sound_position.x = soundPos.x;
+            sound_position.y = soundPos.y;
+            sound_position.z = soundPos.z;
+            FMOD_VECTOR sound_velocity = { 0.0f, 0.0f, 0.0f };
+
+            mresult = msystem->createSound(this->soundObjects[i]->getSource().c_str(), FMOD_3D, 0, &msounds[i]);
+            errorcheck(mresult);
+            mresult = msounds[i]->setMode(FMOD_LOOP_NORMAL);
+            errorcheck(mresult);
+
+            mresult = msounds[i]->set3DMinMaxDistance(0.3f, 5000.0f);
+            errorcheck(mresult);
+
+            //play sound as paused, we will set 3d settings after
+            mresult = msystem->playSound(msounds[i], 0, true, &mchannels[i]);
+            errorcheck(mresult);
+            //set 3d attributes
+            mresult = mchannels[i]->set3DAttributes(&sound_position, &sound_velocity);
+            errorcheck(mresult);
+            //unpause
+            mresult = mchannels[i]->setPaused(false);
+            errorcheck(mresult);
+
+        }
+        else
+        {
+            mresult = msystem->createSound(this->soundObjects[i]->getSource().c_str(), this->soundObjects[i]->getFMODType(), 0, &msounds[i]);
+            errorcheck(mresult);
+            mresult = msounds[i]->setMode(FMOD_LOOP_NORMAL);
+            errorcheck(mresult);
 
 
-    //play sound 
-    mresult = msystem->playSound(msounds[0], 0, false, &mchannels[0]);
-    errorcheck(mresult);
+            //play sound 
+            mresult = msystem->playSound(msounds[i], 0, false, &mchannels[i]);
+            errorcheck(mresult);
 
-    mresult = mchannels[0]->setVolume(0.005f);
-    errorcheck(mresult);
+            mresult = mchannels[i]->setVolume(0.005f);
+            errorcheck(mresult);
+        }
 
 
-    //we need position and velocity vecors for 3d sounds
-    glm::vec3 soundPos = this->soundObjects[1]->getPosition();
-    FMOD_VECTOR sound_position;
-    sound_position.x = soundPos.x;
-    sound_position.y = soundPos.y;
-    sound_position.z = soundPos.z;
-    FMOD_VECTOR sound_velocity = { 0.0f, 0.0f, 0.0f };
 
-    mresult = msystem->createSound(this->soundObjects[1]->getSource().c_str(), this->soundObjects[1]->getFMODType(), 0, &msounds[1]);
-    errorcheck(mresult);
-    mresult = msounds[1]->setMode(FMOD_LOOP_NORMAL);
-    errorcheck(mresult);
+    }
+    
+    if (this->soundObjects.size() == 0)
+    {
+        std::cout << "There were no sound objects to load!\n";
+        system("pause");
+        exit(-1);
+    }
 
-    mresult = msounds[1]->set3DMinMaxDistance(0.3f, 5000.0f);
-    errorcheck(mresult);
 
-    //play sound as paused, we will set 3d settings after
-    mresult = msystem->playSound(msounds[1], 0, true, &mchannels[1]);
-    errorcheck(mresult);
-    //set 3d attributes
-    mresult = mchannels[1]->set3DAttributes(&sound_position, &sound_velocity);
-    errorcheck(mresult);
-    //unpause
-    mresult = mchannels[1]->setPaused(false);
-    errorcheck(mresult);
+
+    
+
 
 
 }
@@ -178,8 +195,11 @@ bool cSoundManager_Imp::loadSoundParameters(std::string configFile)
     std::string token;
     file >> token;
 
+    int line = 0; // Keep track of what line we are reading
+
     while (!file.eof())
     {
+        line++;
 
         if (token.at(0) == '#')
         {
@@ -191,6 +211,14 @@ bool cSoundManager_Imp::loadSoundParameters(std::string configFile)
         cSoudObject* so = new cSoudObject();
 
         int fmod_type = get_FMOD_Type(token);
+
+        if (fmod_type == -1)
+        {
+            std::cout << "Invalid type at line " << line
+                << " in the sound configuration file"
+                << std::endl;
+            return false;
+        }
         
         if (fmod_type == FMOD_3D)
         {
@@ -198,12 +226,19 @@ bool cSoundManager_Imp::loadSoundParameters(std::string configFile)
             file >> token;
             so->setfriendlyName(token);
             float xPos, yPos, zPos;
-            file >> xPos >> yPos >> zPos;
+            if (!(file >> xPos >> yPos >> zPos))
+            {
+                std::cout << "There were a problem reading a model "
+                    "coordinate in the config file at line "
+                    << line << std::endl;
+                return false;
+            }
             glm::vec3 position(xPos, yPos, zPos);
             so->setPosition(position);
             file >> token;
             so->setSource(token);
             this->soundObjects.push_back(so);
+            file >> token;
             continue;
         }
 
@@ -213,7 +248,6 @@ bool cSoundManager_Imp::loadSoundParameters(std::string configFile)
         file >> token;
         so->setSource(token);
         this->soundObjects.push_back(so);
-
         file >> token;
 
     }
@@ -228,6 +262,7 @@ void errorcheck(FMOD_RESULT result) {
     if (result != FMOD_OK)
     {
         fprintf(stderr, "FMOD error! (%d) %s\n", result, FMOD_ErrorString(result));
+        system("pause");
         exit(-1);
     }
 }
@@ -239,6 +274,7 @@ void cSoundManager_Imp::init_fmod() {
     if (this->mresult != FMOD_OK)
     {
         fprintf(stderr, "FMOD error! (%d) %s\n", this->mresult, FMOD_ErrorString(this->mresult));
+        system("pause");
         exit(-1);
     }
 
@@ -247,6 +283,7 @@ void cSoundManager_Imp::init_fmod() {
     if (this->mresult != FMOD_OK)
     {
         fprintf(stderr, "FMOD error! (%d) %s\n", this->mresult, FMOD_ErrorString(this->mresult));
+        system("pause");
         exit(-1);
     }
 
@@ -254,34 +291,35 @@ void cSoundManager_Imp::init_fmod() {
 
 int get_FMOD_Type(std::string token)
 {
-    if (token == "FMOD_DEFAULT"                  ) return FMOD_DEFAULT                 ;    
-    if (token == "FMOD_LOOP_OFF"                 ) return FMOD_LOOP_OFF                ;
-    if (token == "FMOD_LOOP_NORMAL"              ) return FMOD_LOOP_NORMAL             ;
-    if (token == "FMOD_LOOP_BIDI"                ) return FMOD_LOOP_BIDI               ;
-    if (token == "FMOD_2D"                       ) return FMOD_2D                      ;
-    if (token == "FMOD_3D"                       ) return FMOD_3D                      ;
-    if (token == "FMOD_CREATESTREAM"             ) return FMOD_CREATESTREAM            ;
-    if (token == "FMOD_CREATESAMPLE"             ) return FMOD_CREATESAMPLE            ;
-    if (token == "FMOD_CREATECOMPRESSEDSAMPLE"   ) return FMOD_CREATECOMPRESSEDSAMPLE  ;
-    if (token == "FMOD_OPENUSER"                 ) return FMOD_OPENUSER                ;
-    if (token == "FMOD_OPENMEMORY"               ) return FMOD_OPENMEMORY              ;
-    if (token == "FMOD_OPENMEMORY_POINT"         ) return FMOD_OPENMEMORY_POINT        ;
-    if (token == "FMOD_OPENRAW"                  ) return FMOD_OPENRAW                 ;
-    if (token == "FMOD_OPENONLY"                 ) return FMOD_OPENONLY                ;
-    if (token == "FMOD_ACCURATETIME"             ) return FMOD_ACCURATETIME            ;
-    if (token == "FMOD_MPEGSEARCH"               ) return FMOD_MPEGSEARCH              ;
-    if (token == "FMOD_NONBLOCKING"              ) return FMOD_NONBLOCKING             ;
-    if (token == "FMOD_UNIQUE"                   ) return FMOD_UNIQUE                  ;
-    if (token == "FMOD_3D_HEADRELATIVE"          ) return FMOD_3D_HEADRELATIVE         ;
-    if (token == "FMOD_3D_WORLDRELATIVE"         ) return FMOD_3D_WORLDRELATIVE        ;
-    if (token == "FMOD_3D_INVERSEROLLOFF"        ) return FMOD_3D_INVERSEROLLOFF       ;
-    if (token == "FMOD_3D_LINEARROLLOFF"         ) return FMOD_3D_LINEARROLLOFF        ;
-    if (token == "FMOD_3D_LINEARSQUAREROLLOFF"   ) return FMOD_3D_LINEARSQUAREROLLOFF  ;
-    if (token == "FMOD_3D_INVERSETAPEREDROLLOFF" ) return FMOD_3D_INVERSETAPEREDROLLOFF;
-    if (token == "FMOD_3D_CUSTOMROLLOFF"         ) return FMOD_3D_CUSTOMROLLOFF        ;
-    if (token == "FMOD_3D_IGNOREGEOMETRY"        ) return FMOD_3D_IGNOREGEOMETRY       ;
-    if (token == "FMOD_IGNORETAGS"               ) return FMOD_IGNORETAGS              ;
-    if (token == "FMOD_LOWMEM"                   ) return FMOD_LOWMEM                  ;
-    if (token == "FMOD_LOADSECONDARYRAM"         ) return FMOD_LOADSECONDARYRAM        ;
-    if (token == "FMOD_VIRTUAL_PLAYFROMSTART"    ) return FMOD_VIRTUAL_PLAYFROMSTART   ;
+    if      (token == "FMOD_DEFAULT"                  ) return FMOD_DEFAULT                 ;    
+    else if (token == "FMOD_LOOP_OFF"                 ) return FMOD_LOOP_OFF                ;
+    else if (token == "FMOD_LOOP_NORMAL"              ) return FMOD_LOOP_NORMAL             ;
+    else if (token == "FMOD_LOOP_BIDI"                ) return FMOD_LOOP_BIDI               ;
+    else if (token == "FMOD_2D"                       ) return FMOD_2D                      ;
+    else if (token == "FMOD_3D"                       ) return FMOD_3D                      ;
+    else if (token == "FMOD_CREATESTREAM"             ) return FMOD_CREATESTREAM            ;
+    else if (token == "FMOD_CREATESAMPLE"             ) return FMOD_CREATESAMPLE            ;
+    else if (token == "FMOD_CREATECOMPRESSEDSAMPLE"   ) return FMOD_CREATECOMPRESSEDSAMPLE  ;
+    else if (token == "FMOD_OPENUSER"                 ) return FMOD_OPENUSER                ;
+    else if (token == "FMOD_OPENMEMORY"               ) return FMOD_OPENMEMORY              ;
+    else if (token == "FMOD_OPENMEMORY_POINT"         ) return FMOD_OPENMEMORY_POINT        ;
+    else if (token == "FMOD_OPENRAW"                  ) return FMOD_OPENRAW                 ;
+    else if (token == "FMOD_OPENONLY"                 ) return FMOD_OPENONLY                ;
+    else if (token == "FMOD_ACCURATETIME"             ) return FMOD_ACCURATETIME            ;
+    else if (token == "FMOD_MPEGSEARCH"               ) return FMOD_MPEGSEARCH              ;
+    else if (token == "FMOD_NONBLOCKING"              ) return FMOD_NONBLOCKING             ;
+    else if (token == "FMOD_UNIQUE"                   ) return FMOD_UNIQUE                  ;
+    else if (token == "FMOD_3D_HEADRELATIVE"          ) return FMOD_3D_HEADRELATIVE         ;
+    else if (token == "FMOD_3D_WORLDRELATIVE"         ) return FMOD_3D_WORLDRELATIVE        ;
+    else if (token == "FMOD_3D_INVERSEROLLOFF"        ) return FMOD_3D_INVERSEROLLOFF       ;
+    else if (token == "FMOD_3D_LINEARROLLOFF"         ) return FMOD_3D_LINEARROLLOFF        ;
+    else if (token == "FMOD_3D_LINEARSQUAREROLLOFF"   ) return FMOD_3D_LINEARSQUAREROLLOFF  ;
+    else if (token == "FMOD_3D_INVERSETAPEREDROLLOFF" ) return FMOD_3D_INVERSETAPEREDROLLOFF;
+    else if (token == "FMOD_3D_CUSTOMROLLOFF"         ) return FMOD_3D_CUSTOMROLLOFF        ;
+    else if (token == "FMOD_3D_IGNOREGEOMETRY"        ) return FMOD_3D_IGNOREGEOMETRY       ;
+    else if (token == "FMOD_IGNORETAGS"               ) return FMOD_IGNORETAGS              ;
+    else if (token == "FMOD_LOWMEM"                   ) return FMOD_LOWMEM                  ;
+    else if (token == "FMOD_LOADSECONDARYRAM"         ) return FMOD_LOADSECONDARYRAM        ;
+    else if (token == "FMOD_VIRTUAL_PLAYFROMSTART"    ) return FMOD_VIRTUAL_PLAYFROMSTART   ;
+    else return -1;
 }
