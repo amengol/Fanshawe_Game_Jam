63661174e833b8aefbd5a2e6cdd24742c3b8eb27
Jorge <jorge.amengol@gmail.com>
2017-12-08 18:42:58 -0400

    Add Volume, Mute and type of 3D sound (static/mobile)

--- GameEngine_101/cSoundObject.cpp
+++ GameEngine_101/cSoundObject.cpp
@@ -5,6 +5,9 @@ cSoudObject::cSoudObject()
 {
     this->FMOD_Type = FMOD_DEFAULT;
     this->position = glm::vec3(0.0f, 0.0f, 0.0f);
+    this->volume = 1.0f;
+    this->isMute = false;
+    this->movType = 1;
 }
 
 cSoudObject::~cSoudObject()
@@ -32,6 +35,26 @@ void cSoudObject::setFMODType(int type)
     this->FMOD_Type = type;
 }
 
+void cSoudObject::setVolume(float vol)
+{
+    if (vol > 1.0)
+        this->volume = 1.0f;
+    else if (vol < 0.0f)
+        this->volume = 0.0f;
+    else
+        this->volume = vol;
+}
+
+void cSoudObject::setMute(bool mute)
+{
+    this->isMute = mute;
+}
+
+void cSoudObject::setMovType(int type)
+{
+    this->movType = type;
+}
+
 glm::vec3 cSoudObject::getPosition()
 {
     return this->position;
@@ -50,4 +73,19 @@ std::string cSoudObject::getSource()
 int cSoudObject::getFMODType()
 {
     return this->FMOD_Type;
+}
+
+float cSoudObject::getVolume()
+{
+    return this->volume;
+}
+
+bool cSoudObject::getMute()
+{
+    return this->isMute;
+}
+
+int cSoudObject::getMovType()
+{
+    return this->movType;
 }
--- GameEngine_101/cSoundObject.h
+++ GameEngine_101/cSoundObject.h
@@ -16,16 +16,25 @@ public:
     void setfriendlyName(std::string name);
     void setSource(std::string src);
     void setFMODType(int type);
+    void setVolume(float vol);
+    void setMute(bool mute);
+    void setMovType(int type);
     glm::vec3 getPosition();
     std::string getFriendlyName();
     std::string getSource();
     int getFMODType();
+    float getVolume();
+    bool getMute();
+    int getMovType();
 
 private:
     int FMOD_Type;
+    char movType;               // 1 static, 2 mobile
     glm::vec3 position;
     std::string friendlyName;
     std::string source;
+    float volume;
+    bool isMute;
 };
 
 #endif // !_cSoudObject_HG_
