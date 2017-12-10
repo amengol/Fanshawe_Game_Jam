45ce03024e293d5fae8b43399d3d7b56b398ef05
Jorge <jorge.amengol@gmail.com>
2017-12-10 12:26:36 -0400

    Physics now with its own class

---
+++ GameEngine_101/Physics.cpp
@@ -0,0 +1,30 @@
+#include "Physics.h"
+#include <glm/vec3.hpp>
+#include <vector>
+#include "cGameObject.h"
+
+extern std::vector< cGameObject* >  g_vecGameObjects;
+
+// Update the world 1 "step" in time
+void PhysicsStep(double deltaTime)
+{
+    const glm::vec3 GRAVITY = glm::vec3(0.0f, 0.0f, 0.0f);
+
+    // Identical to the 'render' (drawing) loop
+    for (int index = 0; index != ::g_vecGameObjects.size(); index++)
+    {
+        cGameObject* pCurGO = ::g_vecGameObjects[index];
+
+        // Is this object to be updated?
+        if (!pCurGO->bIsUpdatedInPhysics)
+        {
+            continue;
+        }
+
+        pCurGO->Update(deltaTime, GRAVITY);
+
+
+    }//for ( int index... 
+
+    return;
+}
---
+++ GameEngine_101/Physics.h
@@ -0,0 +1,8 @@
+#ifndef _Physics_HG_
+#define _Physics_HG_
+
+// Update the world 1 "step" in time
+void PhysicsStep(double deltaTime);
+
+#endif // !_Physics_HG_
+
