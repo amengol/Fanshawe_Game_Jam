d92dcc5d0a110b9f6a134e19dc8e7db1d2e3f2a7
Jorge <jorge.amengol@gmail.com>
2017-12-10 22:03:28 -0400

    Global 'game' stuff

---
+++ GameEngine_101/globalGameStuff.h
@@ -0,0 +1,36 @@
+#ifndef _globalGameStuff_HG_
+#define _globalGameStuff_HG_
+
+// This is all the global 'game' stuff, so:
+// - not OpenGL rendering stuff (unless we can't help it)
+// - just 'engine' or game play stuff
+
+#include "cGameObject.h"
+#include <vector>
+#include <glm/vec3.hpp>
+#include "cLightManager.h"
+#include "cModelAssetLoader.h"
+#include "cCameraObject.h"
+
+// Remember to #include <vector>...
+extern std::vector< cGameObject* >  g_vecGameObjects;
+
+// Returns 0 or NULL if not found
+cGameObject* findObjectByFriendlyName(std::string friendlyName, std::vector<cGameObject*> &vec_pGameObjects);
+cGameObject* findObjectByUniqueID(unsigned int ID, std::vector<cGameObject*> &vec_pGameObjects);
+
+extern cCameraObject* g_pCamera;            // (GE101_Main.cpp)
+
+
+extern cLightManager*	g_pLightManager;	// (GE101_Main.cpp)
+
+                                            // Super basic physics update function
+void PhysicsStep( double deltaTime );
+
+extern cModelAssetLoader* g_pModelAssetLoader;	// (ModelUtilies.cpp)
+
+// The teapots
+extern cGameObject* pTP0;
+
+
+#endif
