e91f7aeb55f6e6c93a2bd7539af1952f13a5e7a5
Jorge <jorge.amengol@gmail.com>
2017-12-10 22:10:17 -0400

    Add parseStringIntoMultiLine

--- GameEngine_101/cShader.cpp
+++ GameEngine_101/cShader.cpp
@@ -29,3 +29,23 @@ std::string cShaderManager::cShader::getShaderTypeString(void)
 	}
 	return "UNKNOWN_SHADER_TYPE";
 }
+
+void cShaderManager::cShader::parseStringIntoMultiLine(std::string singleLineSouce)
+{
+
+    this->vecSource.clear();
+
+    unsigned int stringsizeX4 = (unsigned int)singleLineSouce.size() * 4;
+    char* charSourceString = new char[stringsizeX4];
+    strcpy(charSourceString, singleLineSouce.c_str());
+
+    char * pCharLine;
+    pCharLine = strtok(charSourceString, "\n");
+    while (pCharLine != NULL)
+    {	// Push back this line
+        this->vecSource.push_back(std::string(pCharLine));
+        pCharLine = strtok(NULL, "\n");
+    }
+    delete[] charSourceString;
+    return;
+}
