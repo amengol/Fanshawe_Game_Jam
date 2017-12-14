23e6247f3f5fc2c6d3d662c1d34e7fa9152ee20f
Jorge <jorge.amengol@gmail.com>
2017-12-14 15:02:40 -0400

    Change ID to Centroid

--- GameEngine_101/sAABB_Triangle.cpp
+++ GameEngine_101/sAABB_Triangle.cpp
@@ -5,6 +5,14 @@ sAABB_Triangle::sAABB_Triangle(glm::vec3 A, glm::vec3 B, glm::vec3 C)
     this->verticeA = A;
     this->verticeB = B;
     this->verticeC = C;
+
+    // We suppose that in the mesh, each 
+    // triangle will have a unique Centroid
+    float centroidX = (A.x + B.x + C.x) / 3;
+    float centroidY = (A.y + B.y + C.y) / 3;
+    float centroidZ = (A.z + B.z + C.z) / 3;
+
+    this->Centroid = glm::vec3(centroidX, centroidY, centroidZ);
 }
 
 void sAABB_Triangle::setTriangle(glm::vec3 A, glm::vec3 B, glm::vec3 C)
@@ -12,4 +20,13 @@ void sAABB_Triangle::setTriangle(glm::vec3 A, glm::vec3 B, glm::vec3 C)
     this->verticeA = A;
     this->verticeB = B;
     this->verticeC = C;
+
+    // We suppose that in the mesh, each 
+    // triangle will have a unique Centroid
+    float centroidX = (A.x + B.x + C.x) / 3;
+    float centroidY = (A.y + B.y + C.y) / 3;
+    float centroidZ = (A.z + B.z + C.z) / 3;
+
+    this->Centroid = glm::vec3(centroidX, centroidY, centroidZ);
+
 }
--- GameEngine_101/sAABB_Triangle.h
+++ GameEngine_101/sAABB_Triangle.h
@@ -5,14 +5,23 @@
 
 struct sAABB_Triangle
 {
-    sAABB_Triangle() : verticeA(0.0f), verticeB(0.0f), verticeC(0.0f) {}
+    sAABB_Triangle() : verticeA(0.0f), verticeB(0.0f), verticeC(0.0f), Centroid(0.0f) {}
     sAABB_Triangle(glm::vec3, glm::vec3, glm::vec3);
+    ~sAABB_Triangle() { return; };
 
     void setTriangle(glm::vec3, glm::vec3, glm::vec3);
 
     glm::vec3 verticeA;
     glm::vec3 verticeB;
     glm::vec3 verticeC;
+
+    // Triandle must have and ID too
+    // the Centroind will do the job
+    // For a mesh, we expect very different Centroids,
+    // so to know if the Triangle is the same, we subtract
+    // two centroid and expect an absolute difference 
+    // no longer than 0.01f to cope with floating point errors
+    glm::vec3 Centroid;
 };
 
 #endif // !_sAABB_Triangle_HG_
