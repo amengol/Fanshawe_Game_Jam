70ce1bf429bf0185bf11b55c643d53d5e4ef4d2c
Jorge <jorge.amengol@gmail.com>
2017-12-17 21:36:39 -0400

    Loads the Delorean

--- GameEngine_101/SceneLoader.cpp
+++ GameEngine_101/SceneLoader.cpp
@@ -21,5 +21,17 @@ void LoadModelsIntoScene(void)
         ::g_vecGameObjects.push_back(theGO);
 	}
 
+    {
+        // Delorean
+        cGameObject* theGO = new cGameObject();
+        theGO->meshName = "Delorean";
+        theGO->bIsUpdatedInPhysics = false;
+        theGO->bIsWireFrame = false;
+        theGO->hasColour = true;
+        theGO->position = glm::vec3(0.0f, 48.0f, 294.0f);
+        theGO->rotateY(180.0f);
+        ::g_vecGameObjects.push_back(theGO);
+    }
+
 	return;
 }
