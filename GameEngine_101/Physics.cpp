57184ed01ab957ebd2d65ce774f2d8b047dd9f1c
Jorge <jorge.amengol@gmail.com>
2017-12-23 17:26:51 -0400

    Firt draft of the collision detection step

--- GameEngine_101/Physics.cpp
+++ GameEngine_101/Physics.cpp
@@ -2,13 +2,16 @@
 #include <glm/vec3.hpp>
 #include <vector>
 #include "cGameObject.h"
+#include "cAABBsManager.h"
+
+extern cAABBsManager* g_pAABBsManager;      // (GE101_Main.cpp)
 
 extern std::vector< cGameObject* >  g_vecGameObjects;
 
 // Update the world 1 "step" in time
 void PhysicsStep(double deltaTime)
 {
-    const glm::vec3 GRAVITY = glm::vec3(0.0f, -10.0f, 0.0f);
+    const glm::vec3 GRAVITY = glm::vec3(0.0f, 0.0f, 0.0f);
 
     // Identical to the 'render' (drawing) loop
     for (int index = 0; index != ::g_vecGameObjects.size(); index++)
@@ -21,7 +24,33 @@ void PhysicsStep(double deltaTime)
             continue;
         }
 
-        pCurGO->Update(deltaTime, GRAVITY);
+        switch(pCurGO->typeOfObject)
+        {
+        case SPHERE:
+        {
+            // Calculate an AABB ID to the GameObject
+            long long GO_ID;
+            if(!g_pAABBsManager->calcID(pCurGO->position, GO_ID, 5.0f))
+            {
+                return;
+            }
+
+            // Check if we have an AABB in that position
+            cAABB* theAABB = nullptr;
+            if(g_pAABBsManager->getAABB(GO_ID, theAABB))
+            {
+                g_pAABBsManager->genDebugLines(GO_ID, 5.0f); // Not sure yet if I will go this way!!
+
+                // TODO: draw my own debug AABB
+            }
+
+            pCurGO->Update(deltaTime, GRAVITY);
+        }
+        default:
+            break;
+        }
+
+        
 
 
     }//for ( int index... 
