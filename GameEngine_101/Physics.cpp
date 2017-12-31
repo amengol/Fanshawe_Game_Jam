1698331718dfec0ec5d0ab422c0b896286d09641
Jorge <jorge.amengol@gmail.com>
2017-12-30 20:26:15 -0400

    Trying to print the triangles surface with the drawCustomGeometry(). Not working yet

--- GameEngine_101/Physics.cpp
+++ GameEngine_101/Physics.cpp
@@ -3,10 +3,13 @@
 #include <vector>
 #include "cGameObject.h"
 #include "cAABBsManager.h"
+#include "cSimpleDebugRenderer.h"
 
 extern cAABBsManager* g_pAABBsManager;      // (GE101_Main.cpp)
 
 extern std::vector< cGameObject* >  g_vecGameObjects;
+
+extern cSimpleDebugRenderer* g_simpleDebug; // (GE101_Main.cpp)
 
 // Update the world 1 "step" in time
 void PhysicsStep(double deltaTime)
@@ -39,11 +42,61 @@ void PhysicsStep(double deltaTime)
             cAABB theAABB(0, 0.0f);
             if(g_pAABBsManager->getAABB(GO_ID, theAABB))
             {
+                std::vector<sVertex> theGeometry;
+
                 for(int i = 0; i < theAABB.AABBsTriangles.size(); i++)
                 {
                     sAABB_Triangle* theTri = theAABB.AABBsTriangles[i];
-                    // Make physics thing
+                    
+                    // Make physics thing                   
+
+                    // Do we need to test for the triangle?
+                    // Check if the Dot product between
+                    // the point and the angle is positive
+                    if(glm::dot(pCurGO->position, theTri->faceNormal) < 0.0f)
+                    {
+                        continue;
+                    }
+                    
+                    // Print the triangle
+
+                    
+                    sVertex tmpGeo;
+
+                    tmpGeo.x = theTri->verticeA.x;
+                    tmpGeo.y = theTri->verticeA.y;
+                    tmpGeo.z = theTri->verticeA.z;
+                    // Find vertices normals
+                    glm::vec3 normal = glm::normalize(glm::cross(theTri->verticeA, theTri->verticeB));
+                    tmpGeo.nx = normal.x;
+                    tmpGeo.ny = normal.y;
+                    tmpGeo.nz = normal.z;
+                    theGeometry.push_back(tmpGeo);
+
+                    tmpGeo.x = theTri->verticeB.x;
+                    tmpGeo.y = theTri->verticeB.y;
+                    tmpGeo.z = theTri->verticeB.z;
+                    // Find vertices normals
+                    normal = glm::normalize(glm::cross(theTri->verticeB, theTri->verticeC));
+                    tmpGeo.nx = normal.x;
+                    tmpGeo.ny = normal.y;
+                    tmpGeo.nz = normal.z;
+                    theGeometry.push_back(tmpGeo);
+
+                    tmpGeo.x = theTri->verticeC.x;
+                    tmpGeo.y = theTri->verticeC.y;
+                    tmpGeo.z = theTri->verticeC.z;
+                    // Find vertices normals
+                    normal = glm::normalize(glm::cross(theTri->verticeC, theTri->verticeA));
+                    tmpGeo.nx = normal.x;
+                    tmpGeo.ny = normal.y;
+                    tmpGeo.nz = normal.z;
+                    theGeometry.push_back(tmpGeo);
                 }
+
+                // Print the mesh
+                g_simpleDebug->drawCustomGeometry(theGeometry);
+
             }
 
             pCurGO->Update(deltaTime, GRAVITY);
--- GameEngine_101/Physics.h
+++ GameEngine_101/Physics.h
@@ -1,6 +1,9 @@
 #ifndef _Physics_HG_
 #define _Physics_HG_
 
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm\gtx\quaternion.hpp>
+
 // Update the world 1 "step" in time
 void PhysicsStep(double deltaTime);
 
