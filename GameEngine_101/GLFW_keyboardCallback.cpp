1bd5e012bf70a142037a7255627a7829018560f2
Jorge <jorge.amengol@gmail.com>
2018-01-07 18:39:20 -0400

    Remove number2 and comment physics properties of number1

--- GameEngine_101/GLFW_keyboardCallback.cpp
+++ GameEngine_101/GLFW_keyboardCallback.cpp
@@ -25,15 +25,7 @@ void key_callback(GLFWwindow* window,
     {
         ::g_pCamera->lockOnGameObject(::g_vecGameObjects[1]);
         //::g_vecGameObjects[1]->bIsUpdatedInPhysics = true;
-    }
-
-    if(key == GLFW_KEY_2 && action == GLFW_PRESS)
-    {
-        //::g_vecGameObjects[1]->bIsUpdatedInPhysics = false;
-        //::g_vecGameObjects[1]->position = glm::vec3(0.0f, 60.0f, 5.0f);
-        //::g_vecGameObjects[1]->vel = glm::vec3(0.0f);
-    }
-        
+    }        
 
     switch(::g_pCamera->getCameraMode())
     {
