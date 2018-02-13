355f119b4d6cde8321c00bdbfb66c19909910888
Jorge <jorge.amengol@gmail.com>
2018-02-13 08:46:46 -0400

    Finish findNeighborsIDs()

--- GameEngine_101/simpleAI.h
+++ GameEngine_101/simpleAI.h
@@ -5,8 +5,20 @@
 
 struct Node
 {
+    Node(int id)
+    {
+        int iX = id / 100;
+        int iY = id - iX;
+
+        // Right handed coord. system:
+        this->position.x = iX;
+        this->position.y = 0.0f;
+        this->position.z = iY;
+    }
+
     int ID;
     std::vector<Node*> connectedNodes;
+    glm::vec3 position;
 };
 
 std::vector<int> findNeighborsIDs(Node* n)
@@ -18,13 +30,86 @@ std::vector<int> findNeighborsIDs(Node* n)
 
     std::vector<int> ret;
 
-    int n1, n2, n3, n4, n5, n6, n7, n8;
+    // Layout:
+    //  n7  n6  n5
+    //  n8  N   n4
+    //  n1  n2  n3
+
+    int n1 = -1;
+    int n2 = -1;
+    int n3 = -1;
+    int n4 = -1;
+    int n5 = -1;
+    int n6 = -1;
+    int n7 = -1;
+    int n8 = -1;
 
     if (x == 0 && y == 0)
     {
-        // first
+        n4 = 100;
+        n5 = 101;
+        n6 = 1;
 
+        ret.push_back(n4);
+        ret.push_back(n5);
+        ret.push_back(n6);
+
+        return ret;
     }
+
+    if (x == 0)
+    {
+        n2 = y - 1;
+        n3 = 100 + y - 1;
+        n4 = 100 + y;
+        n5 = 100 + y + 1;
+        n6 = y + 1;
+
+        ret.push_back(n2);
+        ret.push_back(n3);
+        ret.push_back(n4);
+        ret.push_back(n5);
+        ret.push_back(n6);
+        
+        return ret;
+    }
+
+    if (y == 0)
+    {
+        n4 = (x + 1) * 100;
+        n5 = (x + 1) * 100 + 1;
+        n6 = x * 100 + 1;
+        n7 = (x - 1) * 100 - 1;
+        n8 = (x - 1) * 100;
+       
+        ret.push_back(n4);
+        ret.push_back(n5);
+        ret.push_back(n6);
+        ret.push_back(n7);
+        ret.push_back(n8);
+        
+        return ret;
+    }
+
+    n1 = (x - 1) * 100 + y - 1;
+    n2 = x * 100 + y - 1;
+    n3 = (x + 1) * 100 + y - 1;
+    n4 = (x + 1) * 100 + y;
+    n5 = (x + 1) * 100 + y + 1;
+    n6 = x * 100 + y + 1;
+    n7 = (x - 1) * 100 + y + 1;
+    n8 = (x - 1) * 100 + y;
+
+    ret.push_back(n1);
+    ret.push_back(n2);
+    ret.push_back(n3);
+    ret.push_back(n4);
+    ret.push_back(n5);
+    ret.push_back(n6);
+    ret.push_back(n7);
+    ret.push_back(n8);
+
+    return ret;
 }
 
 std::vector<Node*> makeGrid(int edgeSize, unsigned int rows, unsigned int columns)
@@ -44,7 +129,7 @@ std::vector<Node*> makeGrid(int edgeSize, unsigned int rows, unsigned int column
             int x = i * rows * 100;
             int y = j * columns;
             int ID = x + y;
-            Node* n = new Node();
+            Node* n = new Node(ID);
             nodes.push_back(n);
         }
     }
@@ -76,33 +161,33 @@ std::vector<Node*> makeGrid(int edgeSize, unsigned int rows, unsigned int column
     }
 }
 
-void update_AI_Parameters(cGameObject* theGO, float threshold)
-{
-    float distanceToTarget = glm::length(theGO->target - theGO->position);
-
-    if(distanceToTarget > threshold)
-    {
-
-        glm::vec3 direction = glm::normalize(theGO->target - theGO->position);
-        glm::quat rot1 = glm::rotation(glm::vec3(0.0f, 0.0f, 1.0f), direction);
-
-        theGO->orientation = glm::toMat4(rot1);
-    }
-    else
-    {
-        unsigned int vecSize = theGO->wayPointList.size();
-        if(theGO->currentTargetIndex == vecSize - 1)
-        {
-            theGO->currentTargetIndex = 0;
-            theGO->target = theGO->wayPointList[0];
-        }
-        else
-        {
-            theGO->currentTargetIndex++;
-            theGO->target = theGO->wayPointList[theGO->currentTargetIndex];
-        }
-    }
-}
+//void update_AI_Parameters(cGameObject* theGO, float threshold)
+//{
+//    float distanceToTarget = glm::length(theGO->target - theGO->position);
+//
+//    if(distanceToTarget > threshold)
+//    {
+//
+//        glm::vec3 direction = glm::normalize(theGO->target - theGO->position);
+//        glm::quat rot1 = glm::rotation(glm::vec3(0.0f, 0.0f, 1.0f), direction);
+//
+//        theGO->orientation = glm::toMat4(rot1);
+//    }
+//    else
+//    {
+//        unsigned int vecSize = theGO->wayPointList.size();
+//        if(theGO->currentTargetIndex == vecSize - 1)
+//        {
+//            theGO->currentTargetIndex = 0;
+//            theGO->target = theGO->wayPointList[0];
+//        }
+//        else
+//        {
+//            theGO->currentTargetIndex++;
+//            theGO->target = theGO->wayPointList[theGO->currentTargetIndex];
+//        }
+//    }
+//}
 
 #endif // !_cSimpleAi_Manager_HG_
 
