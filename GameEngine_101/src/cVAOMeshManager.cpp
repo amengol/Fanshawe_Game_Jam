af3f07deaa74fc846774212538c2918b7e0bff3a
Jorge <jorge.amengol@gmail.com>
2018-03-06 18:59:41 -0400

    Add a Dynamic VAO

--- GameEngine_101/src/cVAOMeshManager.cpp
+++ GameEngine_101/src/cVAOMeshManager.cpp
@@ -271,3 +271,245 @@ bool cVAOMeshManager::lookupMeshFromName(std::string name, cMesh &theMesh)
     theMesh = itMesh->second;		// "return" the mesh by reference 
     return true;
 }
+
+void cVAOMeshManager::genDynamicVAO(std::string name, int shaderID)
+{
+    sVAOInfo theVAOInfo;
+
+    // Create a Vertex Array Object (VAO)
+    glGenVertexArrays(1, &(theVAOInfo.VAO_ID));
+    glGenBuffers(1, &(theVAOInfo.vertex_buffer_ID));
+    glGenBuffers(1, &(theVAOInfo.index_buffer_ID));
+   
+    theVAOInfo.friendlyName = name;
+    theVAOInfo.shaderID = shaderID;
+
+    // Store the VAO info by name
+    this->m_mapNameToDynamicVAO[name] = theVAOInfo;
+
+}
+
+void cVAOMeshManager::lookupDynamicVAOFromName(std::string name, sVAOInfo & theVAOInfo)
+{
+    // look up in map for the name of the mesh we want to draw
+    std::map< std::string, sVAOInfo >::iterator itVAO
+        = this->m_mapNameToDynamicVAO.find(name);
+
+    // Did I find something?
+    if (itVAO == this->m_mapNameToDynamicVAO.end())
+    {
+        return;
+    }
+
+    theVAOInfo = itVAO->second;
+}
+
+void cVAOMeshManager::loadMeshIntoDynamicVAO(cMesh& theMesh, std::string SVAOInfoName)
+{
+    // ******************************************************************************
+    //__      __       _              ____         __  __          
+    //\ \    / /      | |            |  _ \       / _|/ _|         
+    // \ \  / /__ _ __| |_ _____  __ | |_) |_   _| |_| |_ ___ _ __ 
+    //  \ \/ / _ \ '__| __/ _ \ \/ / |  _ <| | | |  _|  _/ _ \ '__|
+    //   \  /  __/ |  | ||  __/>  <  | |_) | |_| | | | ||  __/ |   
+    //    \/ \___|_|   \__\___/_/\_\ |____/ \__,_|_| |_| \___|_|   
+    //                                                             
+    // ******************************************************************************
+
+    sVAOInfo theVAOInfo = this->m_mapNameToDynamicVAO[SVAOInfoName];
+    int shaderID = theVAOInfo.shaderID;
+
+    glBindVertexArray(theVAOInfo.VAO_ID);
+    glBindBuffer(GL_ARRAY_BUFFER, theVAOInfo.vertex_buffer_ID);
+
+    // Allocate the global vertex array
+    sVertex* pVertices = new sVertex[theMesh.numberOfVertices];
+
+    for (int index = 0; index < theMesh.numberOfVertices; index++)
+    {
+        pVertices[index].x = theMesh.pVertices[index].x;
+        pVertices[index].y = theMesh.pVertices[index].y;
+        pVertices[index].z = theMesh.pVertices[index].z;
+
+        pVertices[index].r = theMesh.pVertices[index].r;
+        pVertices[index].g = theMesh.pVertices[index].g;
+        pVertices[index].b = theMesh.pVertices[index].b;
+
+        pVertices[index].nx = theMesh.pVertices[index].nx;
+        pVertices[index].ny = theMesh.pVertices[index].ny;
+        pVertices[index].nz = theMesh.pVertices[index].nz;
+
+        // Other additions: texture coords, tangent, and bi-normal
+        pVertices[index].u1 = theMesh.pVertices[index].u1;
+        pVertices[index].v1 = theMesh.pVertices[index].v1;
+        pVertices[index].u2 = theMesh.pVertices[index].u2;
+        pVertices[index].v2 = theMesh.pVertices[index].v2;
+
+        pVertices[index].bx = theMesh.pVertices[index].bx;
+        pVertices[index].by = theMesh.pVertices[index].by;
+        pVertices[index].bz = theMesh.pVertices[index].bz;
+
+        pVertices[index].tx = theMesh.pVertices[index].tx;
+        pVertices[index].ty = theMesh.pVertices[index].ty;
+        pVertices[index].tz = theMesh.pVertices[index].tz;
+
+        pVertices[index].boneID[0] = theMesh.pVertices[index].boneID[0];
+        pVertices[index].boneID[1] = theMesh.pVertices[index].boneID[1];
+        pVertices[index].boneID[2] = theMesh.pVertices[index].boneID[2];
+        pVertices[index].boneID[3] = theMesh.pVertices[index].boneID[3];
+
+        pVertices[index].boneWeights[0] = theMesh.pVertices[index].boneWeights[0];
+        pVertices[index].boneWeights[1] = theMesh.pVertices[index].boneWeights[1];
+        pVertices[index].boneWeights[2] = theMesh.pVertices[index].boneWeights[2];
+        pVertices[index].boneWeights[3] = theMesh.pVertices[index].boneWeights[3];
+    }
+
+    // Copy the local vertex array into the GPUs memory
+    int sizeOfGlobalVertexArrayInBytes = sizeof(sVertex) * theMesh.numberOfVertices;
+    glBufferData(GL_ARRAY_BUFFER, sizeOfGlobalVertexArrayInBytes, pVertices, GL_DYNAMIC_DRAW);
+
+    delete[] pVertices;
+
+    // ******************************************************************************
+    //  _____           _           ____         __  __          
+    // |_   _|         | |         |  _ \       / _|/ _|         
+    //   | |  _ __   __| | _____  _| |_) |_   _| |_| |_ ___ _ __ 
+    //   | | | '_ \ / _` |/ _ \ \/ /  _ <| | | |  _|  _/ _ \ '__|
+    //  _| |_| | | | (_| |  __/>  <| |_) | |_| | | | ||  __/ |   
+    // |_____|_| |_|\__,_|\___/_/\_\____/ \__,_|_| |_| \___|_|   
+    //                                                           
+    // ******************************************************************************
+
+    // Create a Index Array
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, theVAOInfo.index_buffer_ID);
+
+    int numberOfIndices = theMesh.numberOfTriangles * 3;
+
+    unsigned int* indexArray = new unsigned int[numberOfIndices];
+
+    // Copy the triangle data into this 1D array 
+    int triIndex = 0;		// Index into the triangle array (from mesh)
+    int indexIndex = 0;		// Index into the "vertex index" array (1D)
+    for (; triIndex < theMesh.numberOfTriangles; triIndex++, indexIndex += 3)
+    {
+        indexArray[indexIndex + 0] = theMesh.pTriangles[triIndex].vertex_ID_0;
+        indexArray[indexIndex + 1] = theMesh.pTriangles[triIndex].vertex_ID_1;
+        indexArray[indexIndex + 2] = theMesh.pTriangles[triIndex].vertex_ID_2;
+    }
+    int sizeOfIndexArrayInBytes = sizeof(unsigned int) * numberOfIndices;
+
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeOfIndexArrayInBytes, indexArray, GL_DYNAMIC_DRAW);
+
+    // Don't need local array anymore
+    delete[] indexArray;
+
+    // ******************************************************************************
+
+    // At this point, the vertex and index buffers are stored on GPU
+
+    // Now set up the vertex layout (for this shader)
+
+    GLuint vpos_location = glGetAttribLocation(shaderID, "vPos");
+    GLuint vcol_location = glGetAttribLocation(shaderID, "vCol");
+    GLuint vnorm_location = glGetAttribLocation(shaderID, "vNorm");
+    GLuint vUVx2_location = glGetAttribLocation(shaderID, "uvX2");
+    GLuint vTangent_location = glGetAttribLocation(shaderID, "vTangent");
+    GLuint vBitangent_location = glGetAttribLocation(shaderID, "vBitangent");
+    GLuint vBoneIDs_x4_location = glGetAttribLocation(shaderID, "vBoneIDs_x4");
+    GLuint vBoneWeights_x4_location = glGetAttribLocation(shaderID, "vBoneWeights_x4");
+
+    // Size of the vertex we are using in the array.
+    // This is called the "stride" of the vertices in the vertex buffer
+    const unsigned int VERTEX_STRIDE = sizeof(sVertex);
+
+    glEnableVertexAttribArray(vpos_location);
+    const unsigned int OFFSET_TO_X = offsetof(sVertex, x);
+    glVertexAttribPointer(vpos_location, 3,
+                          GL_FLOAT, GL_FALSE,
+                          VERTEX_STRIDE,
+                          reinterpret_cast<void*>(static_cast<uintptr_t>(OFFSET_TO_X)));
+
+    glEnableVertexAttribArray(vcol_location);
+    const unsigned int OFFSET_TO_R = offsetof(sVertex, r);
+    glVertexAttribPointer(vcol_location, 4,
+                          GL_FLOAT, GL_FALSE,
+                          VERTEX_STRIDE,
+                          reinterpret_cast<void*>(static_cast<uintptr_t>(OFFSET_TO_R)));
+
+    glEnableVertexAttribArray(vnorm_location);
+    const unsigned int OFFSET_TO_NX = offsetof(sVertex, nx);
+    glVertexAttribPointer(vnorm_location, 3,
+                          GL_FLOAT, GL_FALSE,
+                          VERTEX_STRIDE,
+                          reinterpret_cast<void*>(static_cast<uintptr_t>(OFFSET_TO_NX)));
+
+    glEnableVertexAttribArray(vUVx2_location);
+    const unsigned int OFFSET_TO_UVs = offsetof(sVertex, u1);
+    glVertexAttribPointer(vUVx2_location, 4,
+                          GL_FLOAT, GL_FALSE,
+                          VERTEX_STRIDE,
+                          reinterpret_cast<void*>(static_cast<uintptr_t>(OFFSET_TO_UVs)));
+
+    glEnableVertexAttribArray(vTangent_location);
+    const unsigned int OFFSET_TO_vTangent = offsetof(sVertex, tx);
+    glVertexAttribPointer(vTangent_location,
+                          3,
+                          GL_FLOAT,
+                          GL_FALSE,
+                          VERTEX_STRIDE,
+                          reinterpret_cast<void*>(static_cast<uintptr_t>(OFFSET_TO_vTangent)));
+
+    glEnableVertexAttribArray(vBitangent_location);
+    const unsigned int OFFSET_TO_vBitangent = offsetof(sVertex, bx);
+    glVertexAttribPointer(vBitangent_location,
+                          3,
+                          GL_FLOAT,
+                          GL_FALSE,
+                          VERTEX_STRIDE,
+                          reinterpret_cast<void*>(static_cast<uintptr_t>(OFFSET_TO_vBitangent)));
+
+    glEnableVertexAttribArray(vBoneIDs_x4_location);
+    const unsigned int OFFSET_TO_vBoneIDs = (unsigned int)offsetof(sVertex, boneID[0]);
+    glVertexAttribPointer(vBoneIDs_x4_location,
+                          4,
+                          GL_FLOAT,
+                          GL_FALSE,
+                          VERTEX_STRIDE,
+                          reinterpret_cast<void*>(static_cast<uintptr_t>(OFFSET_TO_vBoneIDs)));
+
+    glEnableVertexAttribArray(vBoneWeights_x4_location);
+    const unsigned int OFFSET_TO_vBoneWeights_x4 = (unsigned int)offsetof(sVertex, boneWeights[0]);
+    glVertexAttribPointer(vBoneWeights_x4_location,
+                          4,
+                          GL_FLOAT,
+                          GL_FALSE,
+                          VERTEX_STRIDE,
+                          reinterpret_cast<void*>(static_cast<uintptr_t>(OFFSET_TO_vBoneWeights_x4)));
+
+    // Copy the information into the VAOInfo structure
+    theVAOInfo.numberOfIndices = theMesh.numberOfTriangles * 3;
+    theVAOInfo.numberOfTriangles = theMesh.numberOfTriangles;
+    theVAOInfo.numberOfVertices = theMesh.numberOfVertices;
+
+    theMesh.CalculateExtents();
+    theVAOInfo.scaleForUnitBox = theMesh.scaleForUnitBox;
+
+    // Store the VAO info back (updated)
+    this->m_mapNameToDynamicVAO[SVAOInfoName] = theVAOInfo;
+
+    // CRITICAL 
+    // Unbind the VAO first!
+    glBindVertexArray(0);	// 
+
+    // Unbind (release) everything
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    glDisableVertexAttribArray(vcol_location);
+    glDisableVertexAttribArray(vpos_location);
+    glDisableVertexAttribArray(vnorm_location);
+    glDisableVertexAttribArray(vUVx2_location);
+    glDisableVertexAttribArray(vTangent_location);				// in vec3 vTangent;
+    glDisableVertexAttribArray(vBitangent_location);			// in vec3 vBitangent;
+    glDisableVertexAttribArray(vBoneIDs_x4_location);			// in vec4 vBoneIDs_x4;		
+    glDisableVertexAttribArray(vBoneWeights_x4_location);		// 	in vec4 vBoneWeights_x4;
+}
--- GameEngine_101/src/cVAOMeshManager.h
+++ GameEngine_101/src/cVAOMeshManager.h
@@ -18,13 +18,22 @@ public:
 	~cVAOMeshManager();
 	
 	// Take name from mesh for lookup (for rendering)
-	bool loadMeshIntoStaticVAO( cMesh &theMesh, int shaderID, bool bKeepMesh = false );
+	bool loadMeshIntoStaticVAO( cMesh& theMesh, int shaderID, bool bKeepMesh = false );
 
 	// During rendering (aka 'drawing'), get the info we need.
 	bool lookupStaticVAOFromName( std::string name, sVAOInfo &theVAOInfo );
 
     // Return mesh by name
     bool lookupMeshFromName(std::string name, cMesh &theMesh);
+
+    // Generates a Dynamic VAO
+    void genDynamicVAO(std::string name, int shaderID);
+
+    // During rendering (aka 'drawing'), get the info we need.
+    void lookupDynamicVAOFromName(std::string name, sVAOInfo &theVAOInfo);
+
+    // Loads the mesh into the Dynamic VAO refering its SVAOInfo name
+    void loadMeshIntoDynamicVAO(cMesh& theMesh, std::string SVAOInfoName);
 
 	std::map<int, std::string> mapNumberToName;
 
@@ -33,6 +42,8 @@ private:
 	// 1st is what I'm indexing by (i.e. type)
 	// 2nd is what I'm actually storing (the type)
 	std::map< std::string, sVAOInfo > m_mapNameToStaticVAO;
+
+    std::map< std::string, sVAOInfo > m_mapNameToDynamicVAO;
 
     // Meshes that I want to keep around (like the terrain?)
     std::map< std::string, cMesh > m_mapNameToMesh;
