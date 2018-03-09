e05a919d125c0ed1221882977da2c5373e7e77db
Jorge <jorge.amengol@gmail.com>
2018-03-09 08:28:28 -0400

    5 iteration is a resonable value

--- Physics/cPhysicsWorld.cpp
+++ Physics/cPhysicsWorld.cpp
@@ -267,7 +267,7 @@ namespace nPhysics
 
             }//!for (size_t i = 0; i < rbSize; i++)
 
-            cloth->TimeStep(deltaTime, 2);
+            cloth->TimeStep(deltaTime, 5);
         }// !for (size_t i = 0; i < mSoftBody.size(); i++)        
 	}
 
