d5d08092204dd1587b26e259958ad7465af520f8
Jorge <jorge.amengol@gmail.com>
2018-03-13 12:01:46 -0300

    Create a struct to pass the name of default animations

---
+++ GameEngine_101/src/Assimp/sAnimations.h
@@ -0,0 +1,20 @@
+#pragma once;
+#include <string>;
+
+// Stores the name of all supported animations
+struct sAnimations
+{
+    std::string walking;
+    std::string walking_backwards;
+    std::string running;
+    std::string jump;
+    std::string jump_forward;
+    std::string left_strafe;
+    std::string left_strafe_walking;
+    std::string left_turn;
+    std::string left_turn_90;
+    std::string right_strafe;
+    std::string right_strafe_walking;
+    std::string right_turn;
+    std::string right_turn_90;
+};
