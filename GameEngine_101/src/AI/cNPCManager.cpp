2d1c1a5c697e0ede884c7917a066cdfcc76409ac
Jorge <jorge.amengol@gmail.com>
2018-03-15 11:20:27 -0300

    Diasable evaluate for now

--- GameEngine_101/src/AI/cNPCManager.cpp
+++ GameEngine_101/src/AI/cNPCManager.cpp
@@ -13,10 +13,18 @@ cNPCManager::~cNPCManager()
 
 void cNPCManager::Evaluate()
 {
-    std::string error;
-    for (size_t i = 0; i < mNPCs.size(); i++)
-    {
-        g_characterControl.SetControlledCharacter(mNPCs[i]->friendlyName, error);
-        g_characterControl.Forward();
-    }
+    //std::string error;
+    //for (size_t i = 0; i < mNPCs.size(); i++)
+    //{
+    //    g_characterControl.SetControlledCharacter(mNPCs[i]->friendlyName, error);
+    //    g_characterControl.Forward();
+
+    //    // Make it turn to the player
+    //    cGameObject* npc = mNPCs[i];
+    //    glm::vec3 velocity = glm::normalize(mPlayer->position - npc->position) * 1.5f;
+
+
+    //    //npc->rigidBody->rotateY(1.0f);
+    //    npc->rigidBody->SetVelocity(velocity);
+    //}
 }
