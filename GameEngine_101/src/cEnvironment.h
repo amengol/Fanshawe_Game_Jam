ffc736d111a0f8c6d21be9916601821619232fee
Jorge <jorge.amengol@gmail.com>
2018-04-07 18:56:16 -0300

    Set the basic layout for the Environment class

--- GameEngine_101/src/cEnvironment.h
+++ GameEngine_101/src/cEnvironment.h
@@ -4,5 +4,41 @@ class cEnvironment
 public:
     cEnvironment();
     ~cEnvironment();
+
+    enum Mode
+    {
+        CONTINUOUS,
+        STEP
+    };
+
+    enum TimeOfDay
+    {
+        MORNING,
+        NOON,
+        AFTERNOON,
+        NIGHT,
+        MIDNIGHT
+    };
+
+    // Sets the mode
+    void setMode(Mode mode);
+
+    // Advances to the next time of the day
+    void advance(float seconds);
+
+    // Sets the day duration in minutes
+    void setDayDuration(float duration);
+
+    // Start the clock
+    void startClock();
+
+    // Stop the clock
+    void stopClock();
+
+    // Sets a specifc time of the day in hours
+    void setTimeOfDay(float hours);
+
+    // Sets a full time of the day
+    void setTimeOfDay(TimeOfDay tod);
 };
 
