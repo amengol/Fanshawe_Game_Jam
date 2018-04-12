dc5d974226347e182072b0dcb8b9da4fcc8a191f
Jorge <jorge.amengol@gmail.com>
2018-04-12 11:34:36 -0300

    Test convolution kernels

--- GameEngine_101/assets/shaders/newFrag.glsl
+++ GameEngine_101/assets/shaders/newFrag.glsl
@@ -127,6 +127,8 @@ const int FULL_SCENE_RENDER_PASS = 0;
 const int DEFERRED_RENDER_PASS = 1;
 const int FINAL_RENDER_PASS = 99;
 
+const float offset = 1.0 / 3000.0; 
+
 void main()
 {	
 
@@ -364,6 +366,37 @@ void main()
 
 		fragOut_colour.rgb = texture( fullRenderedImage2D, textCoords).rgb;
 		fragOut_colour.a = 1.0f;
+
+		//vec2 offsets[9] = vec2[](
+			//vec2(-offset,  offset), // top-left
+			//vec2( 0.0f,    offset), // top-center
+			//vec2( offset,  offset), // top-right
+			//vec2(-offset,  0.0f),   // center-left
+			//vec2( 0.0f,    0.0f),   // center-center
+			//vec2( offset,  0.0f),   // center-right
+			//vec2(-offset, -offset), // bottom-left
+			//vec2( 0.0f,   -offset), // bottom-center
+			//vec2( offset, -offset)  // bottom-right    
+		//);
+
+		//// Emboss
+		//float kernel[9] = float[](
+		//    -2, -1,  0,
+		//	-1,  1,  1,
+		//	 0,  1,  2
+		//);
+		
+		//vec3 sampleTex[9];
+		//for(int i = 0; i < 9; i++)
+		//{
+		//    sampleTex[i] = vec3(texture(fullRenderedImage2D, textCoords + offsets[i]));
+		//}
+		
+		//vec3 col = vec3(0.0);
+		//for(int i = 0; i < 9; i++)
+		//    col += sampleTex[i] * kernel[i];
+		
+		//fragOut_colour = vec4(col, 1.0);
 	}
 		break;	// end of pass PASS_3_FULL_SCREEN_EFFECT_PASS_2:
 	}
